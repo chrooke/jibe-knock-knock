09d1a8fb0b23d16fe9e03ccd1635a669a652f5a9
Chrooke <chrooke@geekdo.org>
2016-03-10 23:20:57 -0500

    A little more compatibility with the SDK 1.2.2, this time using the "hello world" project as the model.

---
+++ src/index.js
@@ -0,0 +1,51 @@
+"use strict";
+
+let jibo = require('jibo');
+let path = require('path');
+let GuiManager = require('./mim/debug-gui-manager');
+let MimManager = require('./mim/mim-manager');
+
+let Status = jibo.bt.Status;
+
+let root = null;
+
+let blackboard = {};
+let notepad = {};
+
+function start() {
+    let root = jibo.bt.create('../behaviors/KnockKnock', {
+      blackboard: blackboard,
+      notepad: notepad
+    });
+    root.start();
+    let intervalId = setInterval(function() {
+        if (root.status !== Status.IN_PROGRESS) {
+            clearInterval(intervalId);
+            console.log('Behavior tree finished with status ' + root.status);
+        } else {
+            root.update();
+        }
+    }, 33);
+}
+
+jibo.init().then( function(){
+    console.log('Setup');
+    require('./behaviors/debug-behavior');
+    require('./behaviors/mim');
+    require('./behaviors/mim-gui');
+    let eyeElement = document.getElementById('eye');
+    jibo.visualize.createRobotRenderer(
+      eyeElement,
+      jibo.visualize.DisplayType.EYE,
+      function() {
+        start();
+      }
+    );
+    let ui_div = document.getElementById('ui');
+    GuiManager.init(ui_div);
+    MimManager.init();
+    MimManager.setGuiManager(GuiManager);
+//    start();
+}).catch( function(e) {
+      console.error(e);
+});
