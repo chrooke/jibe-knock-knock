51f7bffc07fbdae81ff2a9c5b706a59ce192eaaa
Chrooke <chrooke@geekido.org>
2016-03-20 17:14:30 -0400

    Fixed random joke selection.

--- src/knock-knock/joke-repo.js
+++ src/knock-knock/joke-repo.js
@@ -13,10 +13,23 @@ class JokeRepo {
   }
 
   randomJoke(callback) {
-    this.jokes.find({}).limit(1).skip(Math.floor(Math.random() * this.jokes.count({}))).exec(function (err, doc) {
-              console.log('in find: doc:',doc);
-              callback(err,{ setup: doc[0].setup, punchline: doc[0].punchline });
-    });
+    let _this=this;
+    this.jokes.count({}, function(err,count){
+      let skip = Math.floor(Math.random() * count)
+//      console.log('count:',count);
+//      console.log('skip:',skip)
+      _this.jokes.find({}).limit(1).skip(skip).exec(function (err, doc) {
+                console.log('in find: doc:',doc);
+                callback(err,{ setup: doc[0].setup, punchline: doc[0].punchline });
+      });
+   });
+
+//  This method is theoretically faster for a large database, but fails if it picks a random number
+//    higher than anything in the database. I'll keep this in mind if the performance starts to take a hit.
+//    this.jokes.findOne({ rnd: { $gte: Math.random() } }).sort({ rnd: 1 }).exec(function (err, doc) {
+//      console.log('in find: doc:',doc);
+//      callback(err,{ setup: doc.setup, punchline: doc.punchline });
+//    });;
   }
 
 }
